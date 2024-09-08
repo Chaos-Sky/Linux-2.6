d1530957557d67a7558bed8972b389d5388db1a8
root <1415805848@qq.com>
2024-09-07 19:04:27 -0900

    update

--- src/script/build.sh
+++ src/script/build.sh
@@ -1,5 +1,6 @@
+
 cd _install
-mkdir etc proc sys mnt dev tmp
+mkdir etc proc sys mnt dev tmp root
 touch proc/filesystems
 touch proc/partitions
 mkdir -p etc/init.d
@@ -8,13 +9,17 @@ proc    /proc   proc    defaults        0       0
 tmpfs   /tmp    tmpfs   defaults        0       0
 sysfs   /sys    sysfs   defaults        0       0
 EOF
-cat>>etc/init.d/rcS<<EOF
-echo "Welcome to linux..."
+cat>>root/bashrc<<EOF
 mount -o remount rw /
 mkdir -p /home/share_dir
 mount -t 9p -o trans=virtio hostshare /home/share_dir
 EOF
+cat>>etc/init.d/rcS<<EOF
+/bin/sh root/bashrc
+echo "Welcome to linux version:2.6.34.1"
+EOF
 chmod 755 etc/init.d/rcS 
+chmod 755 root/bashrc
 cat>>etc/inittab<<EOF
 ::sysinit:/etc/init.d/rcS
 ::respawn:-/bin/sh
@@ -29,9 +34,9 @@ sudo mknod tty1 c 4 1
 
 cd ../..
 
-dd if=/dev/zero of=initrd.img bs=4096 count=8192
+dd if=/dev/zero of=initrd.img bs=4096 count=1024
 mkfs.ext3 initrd.img
 mkdir rootfs
 sudo mount -o loop initrd.img rootfs
 sudo cp -rf ./_install/* ./rootfs
-sudo umount ./rootfs
+sudo umount ./rootfs   
